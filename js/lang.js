fdeda318d6395ae5d36d015254ba98c81462ccbb
AlionaMu <alionazimaby@gmail.com>
2023-01-31 17:06:46 +0300

    feat: correct text

--- js/lang.js
+++ js/lang.js
@@ -6,13 +6,14 @@ const i18Obj = {
       'portfolio': 'Portfolio',
       'contacts': 'Contacts',
       'certificates': 'Сertificates',
-      'name': 'Alexey Shishkov',
+      'name': 'Aliona Muhatdinova',
       'position': 'Frontend developer',
-      'aboutMe-info': `I am a web developer with experience building web applications. I have experience in creating a SPA application using React.
-       (participated in the development team in the startup competition from ISsoft.)
-        I constantly improve my technical skills. (completed rs school courses on ts and react)
-         I have experience working in a team. In free time doing my Pet projects, reading
-          books and working on improving my English. Open for your suggestions!`,
+      'aboutMe-info': `Frontend developerer with ability to adapt in both self-starting and
+        collaborative environments while staying focused on achieving high-
+        quality results under strict deadlines.  
+ 
+        I eager to obtain a position that will expand my learning and build upon
+        my developer skills.`,
       'contactMe': 'contact me',
       'inputName': 'Name',
       'email': 'Email',
@@ -26,12 +27,12 @@ const i18Obj = {
       'portfolio': 'Портфолио',
       'contacts': 'Контакты',
       'certificates': 'Сертификаты',
-      'name': 'Алексей Шишков',
+      'name': 'Алёна Мухатдинова',
       'position': 'Фронтенд разработчик',
-      'aboutMe-info': `Я фронтенд разработчик с опытом создания веб приложений. Есть опыт создания SPA-приложения с использованием React.
-       (участвовал в команде разработчиков в конкурсе стартапов от компании ISsoft.)  Постоянно совершенствую свои технические способности.
-       (прошел курсы RS School по ts и react). Есть опыт работы в команде. 
-        В свободное время занимаюсь своими Pet проектами читаю книги и работаю над улучшением своего английского. Открыт для ваших предложений!`,
+      'aboutMe-info': `Фронтенд-разработчик с возможностью адаптироваться, как при самостоятельном работе, так и
+        в команде, оставаясь при этом сосредоточенной на достижении высококачественного результата в строго оговоренные сроки.
+
+        Стремлюсь получить работу, которая расширит мои знания в сфере разработки ПО.`,
       'contactMe': 'свяжитесь со мной',
       'inputName': 'Имя',
       'email': 'Почта',
